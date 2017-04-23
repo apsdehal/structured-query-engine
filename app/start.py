5079d3b5c828227d6815cbd77c67d2378305ba6d
Amanpreet Singh <apsdehal@gmail.com>
2017-04-23 19:20:33 -0400

    Add bootstrapper code to start

--- app/start.py
+++ app/start.py
@@ -1,6 +1,17 @@
+from app.helpers.utils.Bootstrapper import Bootstrapper
+import app.server.frontend.frontend as frontend
+import app.server.indexserver.indexserver as indexserver
+import app.server.docserver.docserver as docserver
+import app.server.idfserver.idfserver as idfserver
+
 
 def main():
-    return
+    bootstrapper = Bootstrapper()
+    config = bootstrapper.bootstrap()
+    docserver.start(config)
+    indexserver.start(config)
+    idfserver.start(config)
+    frontend.start(config)
 
 
 if __name__ == '__main__':
