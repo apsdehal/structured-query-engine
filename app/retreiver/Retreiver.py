5ab1940d209940c360b7b5d2b724815a84bbd45b
Simranjyot Singh <simranjyot1991@gmail.com>
2017-04-23 18:11:01 -0400

    working Retreiver

--- app/retreiver/Retreiver.py
+++ app/retreiver/Retreiver.py
@@ -18,27 +18,31 @@ class Retreiver:
         return result
 
     def query(self, q):
-        data = json.loads(q)['query']
-        if TERM_QUERY in data:
-            items = data[TERM_QUERY].items()
+        try:
+            data = q['query']
+        except KeyError:
+            print('invalid query')
+            return
+        if self.TERM_QUERY in data:
+            items = data[self.TERM_QUERY].items()
             if len(items) > 1:
                 print('[term] query doesnt support multiple fields')
                 return
             [(f,q)] = items
             fields = [f]
             query_strings = [q]
-            query_type = TERM_QUERY
-        elif MATCH_QUERY in data:
-            items = data[MATCH_QUERY].items()
+            query_type = self.TERM_QUERY
+        elif self.MATCH_QUERY in data:
+            items = data[self.MATCH_QUERY].items()
             if len(items) > 1:
                 print('[match] query doesnt support multiple fields')
                 return
             [(f,q)] = items
             fields = [f]
             query_strings = [q]
-            query_type = MATCH_QUERY
-        elif BOOL_QUERY in data:
-            items = data[BOOL_QUERY].items()
+            query_type = self.MATCH_QUERY
+        elif self.BOOL_QUERY in data:
+            items = data[self.BOOL_QUERY].items()
             must_items = items.get('must',[])
             must_not_items = items.get('must_not',[])
             should_items = items.get('should',[])
@@ -51,10 +55,10 @@ class Retreiver:
                 should_query = None
             else :
                 for should_item in should_items :
-                    if len(should_item[MATCH_QUERY].items()) > 1:
+                    if len(should_item[self.MATCH_QUERY].items()) > 1:
                         print('[match] query doesnt support multiple fields')
                         return
-                    [(f,q)] = should_item[MATCH_QUERY].items()
+                    [(f,q)] = should_item[self.MATCH_QUERY].items()
                     should_fields.append(f)
                     should_field_querys.append(q)
 
@@ -65,10 +69,10 @@ class Retreiver:
                 must_query = None
             else :
                 for must_item in must_items :
-                    if len(must_item[MATCH_QUERY].items()) > 1:
+                    if len(must_item[self.MATCH_QUERY].items()) > 1:
                         print('[match] query doesnt support multiple fields')
                         return
-                    [(f,q)] = must_item[MATCH_QUERY].items()
+                    [(f,q)] = must_item[self.MATCH_QUERY].items()
                     must_fields.append(f)
                     must_field_querys.append(q)
 
@@ -79,53 +83,52 @@ class Retreiver:
                 fields.append(f)
                 query_strings.append(q)
 
-            query_type = BOOL_QUERY
+            query_type = self.BOOL_QUERY
         else:
             print('unknown query type')
             return
 
-        with open(self.config.mapping_path) as handler:    
+        with open(self.config['mapping_path']) as handler:    
             mapping = json.load(handler)
 
         scores = {}
 
         for field,query_string in zip(fields,query_strings):
-            if query_type == TERM_QUERY:
+            if query_type == self.TERM_QUERY:
                 query_tokens = [query_string.lower()]
             else :
-                analyzer_type = mapping[field]['movie']['properties'].get('analyzer',STANDARD_ANALYZER)
-                tokenizer = utils.getAnalyzer(analyzer_type)
-                query_tokens = tokenizer.tokenize(query_string.lower())
+                analyzer_type = mapping['movie']['properties'][field].get('analyzer',self.STANDARD_ANALYZER)
+                analyzer = utils.getAnalyzer(analyzer_type)
+                query_tokens = analyzer.analyze(query_string)
 
             query_vector = {}
             document_vectors = {}
-            json_output = {}
             posting_list = []
 
             for token in query_tokens:
                 #if query contains a word twice then it will be ignored second time
                 if token in query_vector:
                     continue
-                query_vector[token] = 1.0*(self.config.term_inv_doc_freq.get(token,1.0))
+                query_vector[token] = 1.0*(self.config['term_inv_doc_freq'].get(token,1.0))
                 try:
-                    tf_list = self.config.tf_list[field].get(token,[])
+                    tf_list = self.config['tf_list'][field].get(token,[])
                 except KeyError:
                     print('field {0} doesnt exist in tf_list'.format(field))
                     break
                 for doc_id,freq in tf_list:
                     if doc_id in document_vectors:
                         inner_dict = document_vectors[doc_id]
-                        inner_dict[token] = freq*(self.term_inv_doc_freq.get(token,1))
+                        inner_dict[token] = freq*(self.config['term_inv_doc_freq'].get(token,1))
                     else:
                         inner_dict = {}
-                        inner_dict[token] = freq*(self.term_inv_doc_freq.get(token,1))
+                        inner_dict[token] = freq*(self.config['term_inv_doc_freq'].get(token,1))
                         document_vectors[doc_id] = inner_dict
 
             for doc_id, document_vector in document_vectors.items():
                 score = self.dot_product(document_vector,query_vector)
                 scores[doc_id] = scores.get(doc_id,0) + score
 
-        for doc_id, score in scores.items:    
+        for doc_id, score in scores.items():    
             posting_list.append([doc_id,score])
 
         posting_list.sort(key=lambda tup: tup[1],reverse=True)
