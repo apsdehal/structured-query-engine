3b30cb5e7d64777a719778e6d36d4f8c7fe43820
Amanpreet Singh <apsdehal@gmail.com>
2017-04-23 22:56:28 -0400

    Add inverted index and doc store loader function

--- app/helpers/utils/General.py
+++ app/helpers/utils/General.py
@@ -1,3 +1,9 @@
+import os
+from app.helpers.utils.Compressor import Compressor
+
+compressor = Compressor()
+
+
 def getAnalyzer(analyzer_type="standard"):
     analyzer_type = analyzer_type.lower()
     if analyzer_type == "standard":
@@ -5,3 +11,30 @@ def getAnalyzer(analyzer_type="standard"):
         analyzer = StandardAnalyzer.StandardAnalyzer()
 
     return analyzer
+
+
+def loadDocStoreAndInvertedIndex(index_name, num_shards, config, mapping):
+    indices_path = config["indices_path"]
+    inverted_indices = {}
+    document_stores = {}
+
+    for type_name in mapping:
+        if type_name not in inverted_indices:
+            inverted_indices[type_name] = []
+
+        if type_name not in document_stores:
+            document_stores[type_name] = []
+
+        file_path = os.path.join(indices_path, index_name)
+        for i in range(num_shards):
+            type_file_path = "%s_%s_%s.tf" % (index_name, type_name, i)
+            type_file_path = os.path.join(file_path, type_file_path)
+            with open(type_file_path, "rb") as f:
+                inverted_indices[type_name].append(compressor.decompress(f.read()))
+
+            type_file_path = "%s_%s_%s.ds" % (index_name, type_name, i)
+            type_file_path = os.path.join(file_path, type_file_path)
+            with open(type_file_path, "rb") as f:
+                document_stores[type_name].append(compressor.decompress(f.read()))
+
+    return document_stores, inverted_indices
