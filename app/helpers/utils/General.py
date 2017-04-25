4b2c2436a40a5d18f2d83b8d8d1c1d29e9d4848b
gamemaker007 <karthik1994@gmail.com>
2017-04-25 16:45:45 -0400

    Removed autovivivification

--- app/helpers/utils/General.py
+++ app/helpers/utils/General.py
@@ -6,16 +6,6 @@ from helpers.utils.Compressor import Compressor
 compressor = Compressor()
 
 
-class AutoVivification(dict):
-    """Implementation of perl's autovivification feature."""
-    def __getitem__(self, item):
-        try:
-            return dict.__getitem__(self, item)
-        except KeyError:
-            value = self[item] = type(self)()
-            return value
-
-
 def getAnalyzer(analyzer_type="standard"):
     analyzer_type = analyzer_type.lower()
     if analyzer_type == "standard":
