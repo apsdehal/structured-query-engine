bfcf7966623abd6ecb43f5e2266c06d2d0f9a357
gamemaker007 <karthik1994@gmail.com>
2017-04-24 23:46:36 -0400

    Added enter and exit function to load and store indexer. removed index as key in tf  and ds as not needed.

--- app/indexer/Indexer.py
+++ app/indexer/Indexer.py
@@ -22,44 +22,63 @@ class AutoVivification(dict):
 
 
 class Indexer:
-    tfTable = AutoVivification()
-    idfTable = AutoVivification()
-    document_store = AutoVivification()
-
+    
     def __init__(self, config, index):
-        self.config = config
-        self.flattener = Flattener(config["indices"][index]["mappings"])
-        self.mapping = self.flattener.getFlattenedMapping()
-        self.tokenizer = Tokenizer(config, self.flattener.getFlattenedMapping())
-        self.compressor = Compressor()
-        self.num_docs = 0
-        self.new_doc_id = 0
-        self.index = index
-        self.index_doc_type = set()
-        self.del_docs = []
-        self.dir_path = os.path.join(self.config["indices_path"], self.index)
-        self.number_of_shards = config["indices"][index]["settings"]["index"]["number_of_shards"]
+        try:
+            with open(index,'rb') as f:                
+                saved_instance = pickle.load(f)                
+                self.__dict__.update(saved_instance)
+                log.info('in init')
+        except:
+            self.tfTable = AutoVivification()
+            self.idfTable = AutoVivification()
+            self.document_store = AutoVivification()
+            self.num_docs = 0
+            self.new_doc_id = 0
+            self.index = index
+            self.index_doc_type = set()
+            self.del_docs = []
+            self.config = config
+            self.flattener = Flattener(config["indices"][index]["mappings"])
+            self.mapping = self.flattener.getFlattenedMapping()
+            self.tokenizer = Tokenizer(config, self.flattener.getFlattenedMapping())
+            self.compressor = Compressor()
+            self.dir_path = os.path.join(self.config["indices_path"], self.index)
+            self.number_of_shards = config["indices"][index]["settings"]["index"]["number_of_shards"]
 
+    def __enter__(self):
+        log.info('in enter')
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.flush_to_file()
+        log.info('in exit')
+        with open(self.index, 'wb') as f:   
+            pickle.dump(self.__dict__, f, pickle.HIGHEST_PROTOCOL)
+        
     def update(self, doc_type, doc_id, doc):
-        deleted = self.delete(doc_type, doc_id)
-        self.degenerate()
-        doc_updated = self.add(doc_type, doc, doc_id, True)
+        if type(doc_id) != int:
+            doc_id = int(doc_id)
+        flattened = self.flattener.flatten(doc_type, doc)
+        inverted_index = self.tokenizer.tokenizeFlattened(doc_type, flattened)
+        self.degenerate_inverted_index(doc_id, doc_type, ii)
+        self.degenerate_doc_store(doc_id, doc_type, doc)
+        doc['doc_id'] = doc_id
+        doc['is_deleted'] = False
+        doc_updated = self.add(doc_type, doc, True)
         log.info(doc_id, ' updated')
-
         return doc_updated
 
     def delete(self, doc_type, doc_id):
         if type(doc_id) != int:
             doc_id = int(doc_id)
-
         if doc_type not in self.index_doc_type:
             log.info('Invalid doc_type')
             return False
-        ds_shard = self.document_store[self.index][doc_type][self.generate_shard_number(doc_id)]
-
+        shard_ds = self.document_store[self.index][doc_type][self.generate_shard_number(doc_id)]
         try:
-            if ds_shard[doc_id]['is_deleted'] is False:
-                ds_shard[doc_id]['is_deleted'] = True
+            if shard_ds[doc_id]['is_deleted'] is False:
+                shard_ds[doc_id]['is_deleted'] = True
                 self.num_docs -= 1
                 self.del_docs.append([doc_type, doc_id])
                 log.info(doc_id, ' deleted')
@@ -72,8 +91,7 @@ class Indexer:
 
         return True
 
-    def add(self, doc_type, doc, doc_id=0, isUpdate=False):
-
+    def add(self, doc_type, doc, isUpdate=False):
         if doc_type not in self.index_doc_type:
             self.document_store[self.index][doc_type] = [dict() for x in range(self.number_of_shards)]
             for field in self.mapping[doc_type]:
@@ -81,28 +99,23 @@ class Indexer:
                     self.tfTable[self.index][doc_type] = [AutoVivification() for x in range(self.number_of_shards)] # defaultdict(lambda: defaultdict(list))
                     self.idfTable[self.index][doc_type][field] = defaultdict(int)
             self.index_doc_type.add(doc_type)
-
-        flattened = self.flattener.flatten(doc_type, doc)
-        inverted_index = self.tokenizer.tokenizeFlattened(doc_type, flattened)
         if isUpdate is False:
             self.new_doc_id += 1
+            self.num_docs += 1
             doc['doc_id'] = self.new_doc_id
-        self.num_docs += 1
-
-        doc['is_deleted'] = False
-        self.generate(doc['doc_id'], doc_type, inverted_index, doc)
-
+            doc['is_deleted'] = False
+        flattened = self.flattener.flatten(doc_type, doc)
+        inverted_index = self.tokenizer.tokenizeFlattened(doc_type, flattened)        
+        self.generate(doc['doc_id'], doc_type, doc, inverted_index)
+        log.info(doc['doc_id'],' added')
         # if self.num_docs % 1000 == 0:
         #     self.flush_to_file()
         #     print('flushed to file')
         return doc
 
-    def generate(self,doc_id, doc_type, ii, doc):
-        # doc_id = self.generate_doc_id()
-        # self.generate_index_number(doc_id)
+    def generate(self,doc_id, doc_type, doc, ii):
         self.generate_inverted_index(doc_id, doc_type, ii)
         self.generate_doc_store(doc_id, doc_type, doc)
-        # self.generate_inverted_doc_frequency(doc_id, doc_type, ii)
 
     def generate_shard_number(self, doc_id):
         return doc_id % self.number_of_shards
@@ -110,34 +123,19 @@ class Indexer:
     def generate_inverted_index(self, doc_id, doc_type, ii):
         for field in self.mapping[doc_type]:
             if self.mapping[doc_type][field].get('index', True):
-                type_field = ii[field]
+                type_field = ii.get(field, [])
                 if all(isinstance(elem, list) for elem in type_field):
                     type_field = [item for sublist in type_field for item in sublist]
-
                 dictionary = Counter(type_field)
                 field_tf = self.tfTable[self.index][doc_type][self.generate_shard_number(doc_id)][field]
                 for key in dictionary:
-                    # posting = [doc_id, dictionary[key]]
                     try:
                         field_tf[key]['num_docs'] += 1
                     except:
                         field_tf[key]['num_docs'] = 1
                     field_tf[key][doc_id] = dictionary[key]
 
-    def generate_inverted_doc_frequency(self, doc_id, doc_type, ii):
-        for field in self.mapping[doc_type]:
-            if self.mapping[doc_type][field].get('index', True):
-                type_field = ii[field]
-                if all(isinstance(elem, list) for elem in type_field):
-                    type_field = [item for sublist in type_field for item in sublist]
-
-                keys = set(type_field)
-                field_idf = self.idfTable[self.index][doc_type][field]
-                for key in keys:
-                    field_idf[key] += 1
-                field_idf['total_number_of_docs'] = self.num_docs
-
-    def generate_doc_store(self, doc_id, doc_type, doc):
+   def generate_doc_store(self, doc_id, doc_type, doc):
         ds_type = self.document_store[self.index][doc_type][self.generate_shard_number(doc_id)]
         try:
             ds_type['num_docs'] += 1
@@ -150,7 +148,6 @@ class Indexer:
         doc_id = int(doc_id)
         return self.document_store[self.index][doc_type][self.generate_shard_number(doc_id)].get(doc_id, {})
 
-
     def degenerate(self):
         for doc_type, doc_id in self.del_docs:
             doc = self.document_store[self.index][doc_type][self.generate_shard_number(doc_id)][doc_id]
@@ -158,7 +155,6 @@ class Indexer:
             inverted_index = self.tokenizer.tokenizeFlattened(doc_type, flattened)
             self.degenerate_inverted_index(doc_id, doc_type, inverted_index)
             self.degenerate_doc_store(doc_id, doc_type, doc)
-
         self.del_docs = []
 
     def degenerate_inverted_index(self, doc_id, doc_type, ii):
@@ -169,15 +165,14 @@ class Indexer:
                     type_field = [item for sublist in type_field for item in sublist]
 
                 dictionary = Counter(type_field)
-                tf_field = self.tfTable[self.index][doc_type][self.generate_shard_number(doc_id)][field]
+                field_tf = self.tfTable[self.index][doc_type][self.generate_shard_number(doc_id)][field]
                 for key in dictionary:
-                    # posting = [doc_id, dictionary[key]]
-                    del tf_field[key][doc_id]
-                    tf_field[key]['num_docs'] -= 1
-                    if (tf_field[key]['num_docs'] == 0):
-                        del tf_field[key]
-                        if not tf_field:
-                            del self.tfTable[self.index][doc_type][self.generate_shard_number(doc_id)][field]
+                    del field_tf[key][doc_id]
+                    field_tf[key]['num_docs'] -= 1
+                    if (field_tf[key]['num_docs'] == 0):
+                        del field_tf[key]
+                        if not field_tf:
+                            del field_tf
 
     def degenerate_doc_store(self, doc_id, doc_type, doc):
         shard_ds = self.document_store[self.index][doc_type][self.generate_shard_number(doc_id)]
@@ -188,26 +183,17 @@ class Indexer:
 
     def flush_to_file(self):
         self.degenerate()
-        for i in self.tfTable:
-            for j in self.tfTable[i]:
-                for k in range(self.number_of_shards):
-                    file_name = i + "_" + j + "_" + str(k) + ".tf"
-                    file_name = os.path.join(self.dir_path, file_name)
-                    with open(file_name, 'wb') as f:
-                        f.write(self.compressor.compress(json.dumps(self.tfTable[i][j][k]).encode()))
-
-        # for i in self.idfTable:
-        #     for j in self.idfTable[i]:
-        #         file_name = i+"_"+j+".idf"
-        #         print(file_name)
-        #         print(self.idfTable[i][j])
-        #         with open(file_name,'wb') as f:
-        #             f.write(self.compressor.compress(json.dumps(self.idfTable[i][j]).encode()))
-
-        for i in self.document_store:
-            for j in self.document_store[i]:
-                for k in range(self.number_of_shards):
-                    file_name = i + "_" + j + "_" + str(k) + ".ds"
-                    file_name = os.path.join(self.dir_path, file_name)
-                    with open(file_name, 'wb') as f:
-                        f.write(self.compressor.compress(json.dumps(self.document_store[i][j][k]).encode()))
+        for j in self.tfTable:
+            for k in range(self.config['num_shards']):
+                file_name = self.index + "_" + j + "_" + str(k) + ".tf"
+                log.info(file_name)
+                # print(self.tfTable[j][k])
+                with open(file_name, 'wb') as f:
+                    f.write(self.compressor.compress(json.dumps(self.tfTable[j][k]).encode()))
+        for j in self.document_store:
+            for k in range(self.config['num_shards']):
+                file_name = self.index + "_" + j + "_" + str(k) + ".ds"
+                log.info(file_name)
+                # print(self.document_store[j][k])
+                with open(file_name, 'wb') as f:
+                    f.write(self.compressor.compress(json.dumps(self.document_store[j][k]).encode()))
