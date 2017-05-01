d343ad9eb1bc72cd64830e6224b8bb84863c434c
Amanpreet Singh <apsdehal@gmail.com>
2017-05-01 00:45:03 -0400

    Add SimpleAnalyzer

---
+++ app/helpers/analyzers/SimpleAnalyzer.py
@@ -0,0 +1,18 @@
+"""
+Simple Analyzer
+
+Takes in a string a splits it whenever it finds a non character word
+"""
+
+from . import BaseAnalyzer
+import re
+
+
+class SimpleAnalyzer(BaseAnalyzer.BaseAnalyzer):
+
+    def __init__(self, config={}):
+        super().__init__(config)
+
+    def analyze(self, query):
+        query = query.lower().strip()
+        return re.split("[^a-zA-Z]", query)
